9ffd15656285517636aaddfa299d3835c3182e93
Unknown <zulberg@mail.csuchico.edu>
2017-03-29 13:51:27 -0700

    Nearing completion

--- project2.js
+++ project2.js
@@ -1,3 +1,72 @@
 function nextPage() {
 window.location.href="page1.html";
 }
+function prevPage() {
+window.location.href="index.html";
+}
+function nextPage2() {
+window.location.href="page2.html";
+}
+function prevPage1() {
+window.location.href="page1.html";
+}
+function nextPage3() {
+window.location.href="page3.html";
+}
+function prevPage2() {
+window.location.href="page2.html";
+}
+function finalPage() {
+window.location.href="results.html";
+}
+function prevPage3() {
+window.location.href="page3.html";
+}
+function testAnswer1() {
+var ans1;
+  if (ans1 == 81648) {
+  document.getElementById("answerGroup").innerHTML="Correct";
+} else {
+  document.getElementById("answerGroup").innerHTML="Incorrect";
+  }
+}
+function testAnswer2() {
+var ans2;
+  if (ans2 == 626160) {
+  document.getElementById("answerGroup").innerHTML="Correct";
+} else {
+  document.getElementById("answerGroup").innerHTML="Incorrect";
+  }
+}
+function testAnswer3() {
+var ans3;
+  if (ans3 == 257144) {
+  document.getElementById("answerGroup").innerHTML="Correct";
+} else {
+  document.getElementById("answerGroup").innerHTML="Incorrect";
+  }
+}
+function testAnswer4() {
+var ans4;
+  if (ans4 == 286187) {
+  document.getElementById("answerGroup").innerHTML="Correct";
+} else {
+  document.getElementById("answerGroup").innerHTML="Incorrect";
+  }
+}
+function testAnswer5() {
+var ans5;
+  if (ans5 == 91620) {
+  document.getElementById("answerGroup").innerHTML="Correct";
+} else {
+  document.getElementById("answerGroup").innerHTML="Incorrect";
+  }
+}
+function testAnswer6() {
+var ans6;
+  if (ans6 == 216630) {
+  document.getElementById("answerGroup").innerHTML="Correct";
+} else {
+  document.getElementById("answerGroup").innerHTML="Incorrect";
+  }
+}

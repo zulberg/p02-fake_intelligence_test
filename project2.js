1fd74a8c57a82826458a6b52acfc3eeac134d844
Unknown <zulberg@mail.csuchico.edu>
2017-03-29 17:33:13 -0700

    Next revision

--- project2.js
+++ project2.js
@@ -23,50 +23,40 @@ function prevPage3() {
 window.location.href="page3.html";
 }
 function testAnswer1() {
-var ans1;
-  if (ans1 == 81648) {
-  document.getElementById("answerGroup").innerHTML="Correct";
-} else {
-  document.getElementById("answerGroup").innerHTML="Incorrect";
+var ans1=document.getElementById("answer1").value;
+  if (ans1 == "81648") {
+  document.getElementById("answer1Group").innerHTML="Correct";
+  document.getElementById("answer1Group").classList.add("has-success");
+  } else {
+  document.getElementById("answer1Group").innerHTML="Incorrect";
+  document.getElementById("answer1Group").classList.add("has-error");
+  document.getElementById("answer1Error").classList.remove("hidden-message");
+  document.getElementById("answer1Error").classList.add("shown-message");
   }
 }
-function testAnswer2() {
-var ans2;
-  if (ans2 == 626160) {
-  document.getElementById("answerGroup").innerHTML="Correct";
-} else {
-  document.getElementById("answerGroup").innerHTML="Incorrect";
-  }
-}
+
 function testAnswer3() {
-var ans3;
-  if (ans3 == 257144) {
-  document.getElementById("answerGroup").innerHTML="Correct";
-} else {
-  document.getElementById("answerGroup").innerHTML="Incorrect";
+var ans3=document.getElementById("answer3").value;
+  if (ans3 == "257144") {
+  document.getElementById("answer3Group").innerHTML="Correct";
+  document.getElementById("answer3Group").classList.add("has-success");
+  } else {
+  document.getElementById("answer3Group").innerHTML="Incorrect";
+  document.getElementById("answer3Group").classList.add("has-error");
+  document.getElementById("answer3Error").classList.remove("hidden-message");
+  document.getElementById("answer3Error").classList.add("shown-message");
   }
 }
-function testAnswer4() {
-var ans4;
-  if (ans4 == 286187) {
-  document.getElementById("answerGroup").innerHTML="Correct";
-} else {
-  document.getElementById("answerGroup").innerHTML="Incorrect";
-  }
-}
+
 function testAnswer5() {
-var ans5;
-  if (ans5 == 91620) {
-  document.getElementById("answerGroup").innerHTML="Correct";
-} else {
-  document.getElementById("answerGroup").innerHTML="Incorrect";
-  }
-}
-function testAnswer6() {
-var ans6;
-  if (ans6 == 216630) {
-  document.getElementById("answerGroup").innerHTML="Correct";
-} else {
-  document.getElementById("answerGroup").innerHTML="Incorrect";
+var ans5=document.getElementById("answer5").value;
+  if (ans5 == "91620") {
+  document.getElementById("answer5Group").innerHTML="Correct";
+  document.getElementById("answer5Group").classList.add("has-success");
+  } else {
+  document.getElementById("answer5Group").innerHTML="Incorrect";
+  document.getElementById("answer5Group").classList.add("has-error");
+  document.getElementById("answer5Error").classList.remove("hidden-message");
+  document.getElementById("answer5Error").classList.add("shown-message");
   }
 }
